50f7e0dd542eb83065466ed8941c8c085630ce34
user name <harshj6680@gmail.com>
2024-10-27 22:14:51 +0530

    Feature: Added projects photos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,56 +5,56 @@ const projects = [
     name: "Government Document Management",
     description:
       "An B.Tech Final Year Project, Created Website with MERN Stack using ReactJS as frontend and NodeJS as backend API. Also used Firebase services for mobile OTP, Storage and User Management, Email etc.",
-    image: "/path/to/government-document-management-image.png",
+    image: "/projects/gov_doc.png",
     link: "https://github.com/harshj-1703/Government-Docs-Manager",
   },
   {
     name: "Government Document Management App",
     description:
       "An B.Tech Final Year Project, Created Flutter Mobile Application that is used to verify Document generated for users in government document management Website with QR Code scanning.",
-    image: "/path/to/government-document-management-image.png",
+    image: "/projects/gov_doc.png",
     link: "https://github.com/harshj-1703/Government-Docs-Manager-Flutter-App",
   },
   {
     name: "HJ Donation",
     description:
       "This project focuses on HJ donation, helping underprivileged individuals by offering old clothing and items. It uses Entity Framework and an MSSQL database. Users can contribute items, and there's an admin panel. Each item can have 4 photos and additional details.",
-    image: "/path/to/rmc-donation-image.png",
+    image: "/projects/hj_donation.png",
     link: "https://github.com/harshj-1703/HJ_Donation_.net",
   },
   {
     name: "Student Interest Subject Choice ChatBot",
     description:
       "An AI project in NLP that helps students select elective subjects for the ICT department with their description and interest.",
-    image: "/path/to/student-interest-chatbot-image.png",
+    image: "/projects/student_choice.png",
     link: "https://github.com/harshj-1703/Student-Elective_Subject-Web-FLASK-AI",
   },
   {
     name: "GET-SET-WORK",
     description:
       "An app connecting job seekers and companies using Flutter and Firebase. It includes Google Ads, Firebase storage, OTP verification, and more.",
-    image: "/path/to/get-set-work-image.png",
+    image: "/projects/getsetwork.png",
     link: "",
   },
   {
     name: "GitaGPT API",
     description:
       "This project allows users to input problems and receive suggestions from the Bhagavad Gita. Built using Flask, it provides a unique way to seek advice through ancient wisdom.",
-    image: "/path/to/geetagpt-image.png",
+    image: "/projects/shreemad_bhagvadgeeta_api.png",
     link: "https://github.com/harshj-1703/Gita_GPT-FLASK-API",
   },
   {
     name: "Shreemad Bhagavad Gita App",
     description:
       "A Flutter app available in both Hindi and English, providing users with access to the teachings of the Bhagavad Gita. This app is available on the Play Store.",
-    image: "/path/to/shreemad-bhagavad-gita-image.png",
-    link: "https://play.google.com/store/apps/details?id=com.shreemad_bhagavadgita.shreemad_bhagavadgita&hl=en",
+    image: "/projects/shreemad_bhagvadgeeta.png",
+    link: "https://play.google.com/store/apps/details?id=com.shreemad_bhagvadgita.shreemad_bhagvadgita&hl=en",
   },
   {
     name: "Chanakya GPT",
     description:
       "This project provides suggestions and advice based on the teachings from the Chanakya Neeti book, helping users apply ancient wisdom to modern challenges.",
-    image: "/path/to/chanakya-gpt-image.png",
+    image: "/projects/chankya_neeti.png",
     link: "https://github.com/harshj-1703/Chanakya-GPT",
   },
 ];
